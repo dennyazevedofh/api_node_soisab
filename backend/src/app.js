aa4a3f6ac5783dbcf346f3ff66ff90c01722a9b9
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-03-11 12:07:09 -0300

    HouseController e DashboardConttroller - validações básicas

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,11 +19,11 @@ class App{
 	}
 
 	middlewares() {
-		this.server.use(express.json());
 		this.server.use(
 			'/files',
 			express.static(path.resolve(__dirname, '..', 'uploads')),
 		);
+		this.server.use(express.json());
 	}
 
 	routes() {
