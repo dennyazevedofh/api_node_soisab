a9585507a12d0fe051461670e5a665b1d16567b1
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-03-18 11:39:42 -0300

    Finalizando o projeto

---
+++ backend/src/controllers/ReserveController.js
@@ -0,0 +1,59 @@
+import Reserve from '../models/Reserve';
+import User from '../models/User';
+import House from '../models/House';
+import mongoose from 'mongoose';
+
+class ReserveController { 
+	async index(req, res) {
+		const { user_id } = req.headers;
+		const reservas = await Reserve.find({ user: user_id }).populate('house');
+		return res.json(reservas);
+	}
+
+	async store(req, res) {
+		const { user_id } = req.headers;
+		const { house_id } = req.params;
+		const { date } = req.body;
+		let house = {};
+		if (mongoose.Types.ObjectId.isValid(house_id)) {
+			house = await House.findById(house_id);
+			if (!house) {
+				return res.status(400).json({ error: 'Essa casa não existe.' });
+			}
+		} else {
+			return res.status(406).json({ error: 'Código da casa inválido!' });
+		}
+		
+		if (house.status !== true) {
+			return res.status(400).json({ error: 'Solicitação indisponivel.' });
+		}
+		const user = await User.findById(user_id);
+		if (String(user._id) === String(house.user)) {
+			return res.status(401).json({ error: 'Reserva não permitida.' });
+		}
+		const reserve = await Reserve.create({
+			user: user_id,
+			house: house_id,
+			date,
+		});
+		await reserve.populate('house');
+		await reserve.populate('user');
+		return res.json(reserve);
+	}
+
+	async destroy(req, res) {
+		const { reserve_id } = req.body;
+		if (mongoose.Types.ObjectId.isValid(reserve_id)) {
+			const reserva = await House.findById(reserve_id);
+			if (!reserva) {
+				return res.status(400).json({ error: 'Essa reserva não existe.' });
+			}
+		} else {
+			return res.status(406).json({ error: 'Código da reserva inválido!' });
+		}
+		await Reserve.findByIdAndDelete({ _id: reserve_id });
+		return res.send();
+	}
+}
+
+export default new ReserveController();
--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,7 @@ import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -16,5 +17,9 @@ routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.upda
 routes.delete('/houses/:house_id', HouseController.destroy);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserves/cancel', ReserveController.destroy);
 
 export default routes;
